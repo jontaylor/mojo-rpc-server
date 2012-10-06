0fe1105a4a597f303302d368078032415a2e7ec9
Jon Taylor <jon@stackhaus.com>
2012-10-06 19:13:49 +0100

    Remove dumpers

--- lib/MojoRPC/Server/Parameters/JSON.pm
+++ lib/MojoRPC/Server/Parameters/JSON.pm
@@ -1,11 +1,14 @@
 package MojoRPC::Server::Parameters::JSON;
 use Mojo::Base 'MojoRPC::Server::Parameters';
 use JSON::XS;
+use URI::Escape;
 
 sub decode {
   my $self = shift;
   my $json = JSON::XS->new->allow_nonref();
-  return @{$json->decode($self->delegated_by->parameters)};
+  my @params = @{$json->decode(uri_unescape($self->delegated_by->parameters))};
+
+  return @params;
 }
 
 1;
