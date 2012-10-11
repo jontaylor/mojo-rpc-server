2472ea41868a0e1b7173e15765ee60733ebc2610
Jon Taylor <jon@stackhaus.com>
2012-10-11 19:05:10 +0100

    Tweak for returning an object in list context

--- lib/MojoRPC/Server/ResponseFormatter.pm
+++ lib/MojoRPC/Server/ResponseFormatter.pm
@@ -8,12 +8,22 @@ sub json {
   my $self = shift;
 
   my $response_hash = {};
-  $response_hash->{data} = $self->method_return_value;
+
+  #Hack for array of 1 item
+  if(
+    ref($self->method_return_value) eq "ARRAY" &&
+    scalar(@{$self->method_return_value}) == 1
+  ) {
+    $self->method_return_value( $self->method_return_value->[0] );
+  }
+
   if(blessed $self->method_return_value) {
     my $id = $self->method_return_value->can('id') ? $self->method_return_value->id() : $self->method_return_value->{id};
     $response_hash->{id} = $id if $id;
     $response_hash->{class} = ref($self->method_return_value);
   }
+
+  $response_hash->{data} = $self->method_return_value;
 
   return $response_hash;
 }
