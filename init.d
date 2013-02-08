b32456bd4462329c5becd5b7ea0022ccea27dde1
Jon Taylor <jon@stackhaus.com>
2013-02-08 09:54:26 +0000

    Add an example init script

---
+++ init.d
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# source function library
+. /etc/init.d/functions
+
+RETVAL=0
+prog="Mojo-RPC Server"
+working_directory="/opt/mojo-rpc-server"
+application_file="./script/mojo-rpc"
+mojo_mode="production"
+
+start() {
+        echo -n $"Starting $prog: "
+        if [ $UID -ne 0 ]; then
+                RETVAL=1
+                failure
+        else
+                cd $working_directory
+                MOJO_MODE=$mojo_mode hypnotoad $application_file
+                RETVAL=$?
+        [ $RETVAL -eq 0 ] && success || failure
+        fi;
+        echo 
+        return $RETVAL
+}
+
+stop() {
+        echo -n $"Stopping $prog: "
+        if [ $UID -ne 0 ]; then
+                RETVAL=1
+                failure
+        else
+                cd $working_directory
+                MOJO_MODE=$mojo_mode hypnotoad $application_file --stop
+                RETVAL=$?
+                [ $RETVAL -eq 0 ] && rm -f /var/lock/subsys/milter-greylist
+        [ $RETVAL -eq 0 ] && success || failure
+        fi;
+        echo
+        return $RETVAL
+}
+
+restart(){
+    stop
+    start
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    restart
+        ;;
+  hotdeploy)
+    start
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop|restart|hotdeploy}"
+    RETVAL=1
+esac
+
+exit $RETVAL
